4e629e7a2f6f0aff9d99c99976fa372eecb039c9
Pavel <p.a.kadantsev@gmail.com>
2017-11-01 15:03:11 +0300

    Minor updates

--- check_sites_health.py
+++ check_sites_health.py
@@ -18,17 +18,25 @@ def define_charset(filepath):
     return file_charset
 
 def load_urls4check(filepath):
-    urls_list = []
+    all_urls_data = []
     if not os.path.isfile(filepath):
         return None
     with open(filepath, 'r', encoding = 'utf-8') as file_content:
-        for url_line in file_content:
-            url = url_line.replace('\n','')
-            opened_urls_list.append(url)
-    print(opened_urls_list) 
+        for line in file_content:
+            url = line.replace('\n','')
+            url_info = {}
+            url_info['url'] = url
+            url_info['status_code'] = None
+            url_info['reason_phrase'] = None
+            url_info['payment'] = None
+            all_urls_data.append(url_info)
+        print(all_urls_data) 
     #return opened_urls_list
     
 def is_server_respond_with_200(url):
+    for url_info in load_urls4check(filepath):
+        
+    
     pass 
     
 
@@ -39,4 +47,5 @@ if __name__ == '__main__':
     #path = return_arg().filepath
     load_urls4check(return_arg().filepath)
     
-    [{'url':'devman.org','code_200':'type_boolean_here', 'domain_payment > 1 month': 'type_boolean_here'}, {}, {}]
+    #[{'url':'devman.org','code_200':'type_boolean_here', 'domain_payment > 1 month': 'type_boolean_here'}, {}, {}]
+    
