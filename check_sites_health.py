d0e310d4f6f5385479bd7193cee8e67395ca4661
Pavel <p.a.kadantsev@gmail.com>
2017-11-02 14:58:42 +0300

    Small improvements

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,14 @@
+from datetime import datetime
 import argparse
 #import requests
-import chardet
+#import chardet
+#import whois
 import os
+import re
 
-def return_arg():
+def return_args():
     parser = argparse.ArgumentParser(prog = 'Site Heath Check Program.',
-                                     description = 'Console utility for checking resource responce and domain payment.')
+                                     description = 'Console utility for checking URL resource responce code and domain expiration date.')
     parser.add_argument('filepath', help = 'Path to file containing URLs list', type = str)
     args = parser.parse_args()
     return args
@@ -17,15 +20,26 @@ def define_charset(filepath):
         file_charset = chardet.detect(ulrs_list_file.read()).get('encoding')
     return file_charset
 
-def load_urls4check(filepath, charset):
-    all_urls_data = []
+def load_urls4check(filepath, charset = 'utf-8'):
+    valid_urls, invalid_urls = [], []
+    urls = {}
     if not os.path.isfile(filepath):
         return None
     with open(filepath, 'r', encoding = charset) as file_content:
-        #return file_content.read().splitlines()
-        print(file_content.read().splitlines())
-    
-def get_server_response(url):
+        for url in file_content.read().splitlines():
+            if re.compile('http').match(url) is not None:
+                valid_urls.append(url)
+                urls['valid'] = valid_urls 
+            else:
+                invalid_urls.append(url)
+                urls['invalid'] = invalid_urls
+        #return urls
+        print(urls)
+        if urls is True:
+            print('Yes')
+        else:
+            print('False')
+def get_server_response_code(url):
     try:
         response_code = requests.get(url).status_code
         return response_code
@@ -40,21 +54,20 @@ def check_domain_expiration_date(domain_name):
         return domain_expiration_dt[0].date()
     else:
         return None
-
-
+        
+#remainig_subscription_days = (domain_expiration_dt[0] - datetime.now()).date()
 
 if __name__ == '__main__':
-    #path = return_arg().filepath
-
-    load_urls4check(return_arg().filepath, define_charset(return_arg().filepath))
-
-
-        #     url = line.replace('\n','')
-        #     url_info = {}
-        #     url_info['url'] = url
-        #     url_info['status_code'] = None
-        #     url_info['reason_phrase'] = None
-        #     url_info['payment'] = None
-        #     all_urls_data.append(url_info)
-        # print(all_urls_data) 
-    #return opened_urls_list
+    load_urls4check(return_args().filepath)
+    
+#if __name__ == '__main__':
+#    urls_loaded_from_file = load_urls4check(return_args().filepath,
+#                                            define_charset(return_args().filepath))
+#    if urls_loaded_from_file: 
+#       for url in urls_loaded_from_file['valid']:
+#           responce_code = get_server_response_code(url)
+#           expiration_date = check_domain_expiration_date(url)
+#           days_till_expiration = expiration_date - datetime.now()).date() 
+#               
+#    else:
+#       print('No valid URLs or empty file')    
