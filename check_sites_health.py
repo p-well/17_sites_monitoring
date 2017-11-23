fe601237ec8fe24bb0603f8e2c8467be30168077
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2017-11-24 01:45:41 +0300

    Ready to review

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,26 +2,17 @@ from datetime import date, datetime
 import os
 import argparse
 import requests
-import chardet
 import whois
 
 
 def return_args():
-    parser = argparse.ArgumentParser(prog = 'Site Heath Check Program.')
-    parser.add_argument('filepath', \
-                        help = 'Path to file with URLs list', type=str)
+    parser = argparse.ArgumentParser(prog='Site Heath Check Program.')
+    parser.add_argument('filepath',
+                        help='Path to file with URLs list', type=str)
     args = parser.parse_args()
     return args
 
-    
-def define_charset(filepath):
-    if not os.path.isfile(filepath):
-        return None
-    with open(filepath,'rb') as ulrs_list_file:
-        file_charset = chardet.detect(ulrs_list_file.read()).get('encoding')
-    return file_charset
 
-    
 def load_urls4check(filepath):
     valid_urls, invalid_urls = [], []
     urls = {}
@@ -31,7 +22,7 @@ def load_urls4check(filepath):
         for url in file_content.read().splitlines():
             if url.startswith('http'):
                 valid_urls.append(url)
-                urls['valid'] = valid_urls 
+                urls['valid'] = valid_urls
             else:
                 invalid_urls.append(url)
                 urls['invalid'] = invalid_urls
@@ -45,7 +36,7 @@ def get_server_response_code(url):
     except requests.exceptions.RequestException:
         return None
 
-        
+
 def get_domain_expiration_date(domain_name):
     domain_expiration_dt = whois.whois(url).expiration_date
     if isinstance(domain_expiration_dt, datetime):
@@ -54,58 +45,35 @@ def get_domain_expiration_date(domain_name):
         return domain_expiration_dt[0]
     else:
         return None
- 
 
 
- 
-# if __name__ == '__main__':
-#     urls_from_file = load_urls4check(return_args().filepath,
-#                                      define_charset(return_args().filepath))
-#     print(urls_from_file)
-#     if urls_from_file:
-#         for count, url in enumerate(urls_from_file.get('valid'), start=1):
-#             response_code = get_server_response_code(url)
-#             exp_date_raw = check_domain_expiration_date(url)
-#             days_till_expiration = (exp_date_raw.date() - date.today())
-#             exp_date_formatted = datetime.strftime(exp_date_raw, '%d-%m-%Y')
-            
-#             print('{}. Resource: {} \n   Status code: {} \n   Expiration date: {} \n   Remaing time: {} days \n'
-#                  .format(count, url, response_code, exp_date_formatted, days_till_expiration.days))
-#             if days_till_expiration.days < 30:
-#                 print('Warning! Domain name will expire soon.')
-#         if urls_from_file.get('invalid'):
-#             print('The follwing items have not been checked. Specify protocop in URL and try again.')
-#             for count, url in enumerate(urls_from_file['invalid'], start=1):
-#                 print('{}. {}').format(count, url)
-#     else:
-#         print('\nNo valid URLs or empty file')
+def print_resource_health_data(url, status_code=None,
+                               expiration_date=None,
+                               remaining_days=None,
+                               valid_urls=None,
+                               invalid_urls=None):
+    if status_code == 200:
+        print("\nResource: {} | Status Code: {} | Available until: {}"
+              .format(url, status_code, expiration_date))
+    else:
+        print('\nResource %s FAILED with status code %s' % (url, status_code))
+    if remaining_days < 30:
+        print('Warning! Domain name will expire in %s days' % remaining_days)
 
 
-
-# def print_resource_health_info(url, status_code=None,
-#                                expiration_date=None,
-#                                remaining_days=None,
-#                                valid_urls=None,
-#                                invalid_urls=None):
-#     if status_code == 200:
-#         print("\nResource: {} Status Code: {} Available until: {}").format(url, status_code, expiration_date)
-#     else:
-#         print('Resource  %s  failed' % url)
-#     if remaining_days < 30:
-#         print('Warning! Domain name will expire in %s days' % remaining_days)
-
-
-
-# if __name__ == '__main__':
-#     urls_from_file = load_urls4check(return_args().filepath)
-#     if urls_from_file.get('valid'):
-#         for url in urls_from_file:
-#             response_code = get_server_response_code(url)
-#             exp_date_raw = get_domain_expiration_date(url)
-#             remaining_days = (exp_date_raw.date() - date.today())
-#             exp_date_formatted = datetime.strftime(exp_date_raw, '%d-%m-%Y')
-#             print_resource_health_info(url, response_code,
-#                                        exp_date_formatted,
-#                                        remaining_days)
-
-
+if __name__ == '__main__':
+    urls_for_check = load_urls4check(return_args().filepath)
+    if urls_for_check.get('valid'):
+        for url in urls_for_check['valid']:
+            response_code = get_server_response_code(url)
+            exp_date_raw = get_domain_expiration_date(url)
+            remaining_days = (exp_date_raw.date() - date.today()).days
+            exp_date_formatted = datetime.strftime(exp_date_raw, '%d-%m-%Y')
+            print_resource_health_data(url, response_code,
+                                       exp_date_formatted,
+                                       remaining_days)
+    if urls_for_check.get('invalid'):
+        print('\n\nThe following items are not checked. Specify protocop \
+and try again.\n')
+        for url in urls_for_check['invalid']:
+            print(url)
